ce8a520fe7907c0f654228031b5934bf22f3c675
bwsrcurtis <bwsrcurtis@gmail.com>
2023-04-23 21:25:52 -0400

    Formatting

--- Algorithms/Sorting/mergeSort.js
+++ Algorithms/Sorting/mergeSort.js
@@ -13,6 +13,7 @@ function mergeSort(arr) {
 	return merge(sortedLeft, sortedRight);
 }
 
+
 // compare the arrays item by item and return the concatenated result
 function merge(left, right) {
 	let result = []
